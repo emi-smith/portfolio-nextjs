089b0ee0fc1f1344eb91989d59bbfde452c5ebed
emi-smith <87035540+emi-smith@users.noreply.github.com>
2022-11-28 19:26:58 -0700

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-     import Head from 'next/head'
+ import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -11,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <h1 className="text-3xl font-bold underline">
+        Hello with tailwind
+      </h1>
     </div>
   )
 }
