3a7c72bb01460a37b5d66f57c0d6426089171ed0
emi-smith <87035540+emi-smith@users.noreply.github.com>
2022-12-15 22:26:51 -0700

    Added sticky-note project page

---
+++ pages/sticky-notes.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const stickynotes = () => {
+	return (
+		<div>
+			<h1>Sticky Notes</h1>
+			<p>Update this page!!</p>
+			<div>
+				<Link href='https://sticky-notes-gilt.vercel.app/'>View Project</Link>
+			</div>
+		</div>
+	);
+};
+
+export default stickynotes;
