fa268063769e906fb0e8771433acf972db9dedb7
emi-smith <87035540+emi-smith@users.noreply.github.com>
2023-06-07 21:59:52 -0600

    Update sticky notes page

--- pages/sticky-notes.jsx
+++ pages/sticky-notes.jsx
@@ -1,63 +1,53 @@
 import React from "react";
 import Image from "next/image";
-import stickynoteprojectimage from "../public/assets/projects/sticky-note-project-image.png";
+import StickyNoteImg from "../public/assets/projects/sticky-note-project-image.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
-const stickynotes = () => {
+const stickyNotes = () => {
 	return (
 		<div className='w-full'>
 			<div className='w-screen h-[50vh] relative'>
 				<div className='absolute top-0 left-0 w-full h-[50vh] bg-black/60 z-10' />
-				{/* <Link href='https://sticky-notes-gilt.vercel.app/'>View Project</Link> */}
 				<Image
 					className='object-cover w-full h-[50vh] absolute z-1'
 					Layout='fill'
-					src={stickynoteprojectimage}
+					src={StickyNoteImg}
 					alt='/'
 				/>
 				<div className='absolute top-[70%] max-w-[1240px] px-4 w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-					<h2 className='py-2'>Sticky Notes</h2>
+					<h2 className='py-2'>Simple Sticky Notes</h2>
 					<h3>HTML / CSS / JavaScript</h3>
 				</div>
 			</div>
 
 			<div className='max-w-[1240px] mx-auto p-2 px-4 grid md:grid-cols-5 gap-8 py-8'>
 				<div className='col-span-4'>
-					<p className=' uppercase py-2 text-xl tracking-widest text-[#5651e5]'>
+					<p className=' uppercase py-2 text-xl tracking-widest text-[#62806a]'>
 						Project
 					</p>
 					<h2 className='pb-2'>Overview</h2>
-					<p>
-						This app was built using React JS and is hosted on Firebase. Users
-						are able to search properties based on an Address, City, or ZIP code
-						to retrieve a list of active properties currently for sale. You will
-						be able to view property information as well as the specific
-						location of the property integrated with the Google Maps API. User
-						authentication is available so you can signup and signin to your
-						account with an email address in order to save your favorite
-						properties. This is made possible with Zillow API.
-					</p>
+					<p>Add app information here.</p>
 
 					<div className='grid grid-cols-2 justify-items-center pt-5 pb-10'>
 						<a
-							href='https://github.com/fireclint/property-finder'
+							href='https://github.com/emi-smith/sticky-notes'
 							target='_blank'
 							rel='noreferrer'
 						>
-							<button className='px-8 py-2 mt-4 mr-8'>Code</button>
+							<button className='px-8 py-2 mt-4 mr-8'>View Code</button>
 						</a>
 						<a
-							href='https://property-finder-development.web.app/'
+							href='https://sticky-notes-gilt.vercel.app/'
 							target='_blank'
 							rel='noreferrer'
 						>
-							<button className='px-8 py-2 mt-4'>Demo</button>
+							<button className='px-8 py-2 mt-4'>View Project</button>
 						</a>
 					</div>
 				</div>
 				<div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
-					<div className='p-2'>
+					<div className='p-4'>
 						<p className='text-center font-bold pb-2'>Technologies</p>
 						<div className='grid grid-cols-3 md:grid-cols-1'>
 							<p className='text-gray-600 py-2 flex items-center'>
@@ -73,11 +63,11 @@ const stickynotes = () => {
 					</div>
 				</div>
 				<Link href='/#projects'>
-					<p className='underline cursor-pointer'>Back</p>
+					<p className='cursor-pointer text-[#62806a]'>Back</p>
 				</Link>
 			</div>
 		</div>
 	);
 };
 
-export default stickynotes;
+export default stickyNotes;
