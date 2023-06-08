08331aa030275ed4688580aa11cf2e0c8619a140
emi-smith <87035540+emi-smith@users.noreply.github.com>
2023-06-07 21:58:11 -0600

    Update project item component

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const ProjectItem = ({ title, backgroundImg, projecturl }) => {
+const ProjectItem = ({ title, technology, backgroundImg, projecturl }) => {
 	return (
 		<div
 			id='ProjectId'
-			className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5654e5] to-[#709eff]'
+			className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#62806a] to-[#9ca89a]'
 		>
 			<Image
 				className='rounded-xl group-hover:opacity-10'
@@ -17,12 +17,10 @@ const ProjectItem = ({ title, backgroundImg, projecturl }) => {
 				<h3 className='text-2xl text-white tracking-wider text-center'>
 					{title}
 				</h3>
-				<p className='pb-4 pt-2 text-white text-center'>
-					HTML, CSS, Javascript
-				</p>
+				<p className='pb-4 pt-2 text-white text-center'>{technology}</p>
 				<Link href={projecturl}>
 					<p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
-						More Info
+						More Details
 					</p>
 				</Link>
 			</div>
