5204abc6da04a75666299458f73d065fed349428
emi-smith <87035540+emi-smith@users.noreply.github.com>
2023-08-03 08:17:12 -0600

    Remove sections, update header font, weight, color

--- components/EducationSkills.jsx
+++ components/EducationSkills.jsx
@@ -3,86 +3,58 @@ import { RiRadioButtonFill } from "react-icons/ri";
 
 const Education = () => {
 	return (
-		<div className='col-span-5 lg:col-span-2 shadow-xl shadow-gray-400 rounded-xl px-6 py-6 bg-gradient-to-br from-[#ecf0f3] to-[#9ca89a] '>
+		<div className='col-span-5 lg:col-span-2 shadow-xl shadow-gray-400 rounded-xl px-0 sm:px-6 py-6 bg-gradient-to-br from-[#ecf0f3] to-[#9ca89a] '>
 			<div className='p-6'>
-				<p className=' uppercase py-2 text-xl tracking-widest text-[#62806a]'>
+				<p className=' uppercase py-2 text-2xl tracking-widest text-[#628069] '>
 					Education
 				</p>
 				<div className='px-4 pb-2'>
 					<p className='pt-2 text-xl'>Dakota State University</p>
-					<p className='px-2 text-[#9ca89a]'>Health Information Technology</p>
-					<p className='px-2 text-xs text-gray-500'>
+					<p className='text-[#628069] italic'>Health Information Technology</p>
+					<p className='text-xs text-gray-500'>
 						Associate&apos;s of Applied Science
 					</p>
-					<p className='px-2 text-xs text-gray-500'>2014 - 2017</p>
 				</div>
 				<div className='px-4 pb-2'>
 					<p className='pt-2 text-xl'>CollegeAmerica</p>
-					<p className='px-2 text-[#9ca89a]'>
+					<p className='text-[#628069] italic'>
 						Business Management & Accounting
 					</p>
-					<p className='px-2 text-xs text-gray-500'>
+					<p className='text-xs text-gray-500'>
 						Associate&apos;s of Applied Science
 					</p>
-					<p className='px-2 text-xs text-gray-500'>2009 - 2012</p>
-				</div>
-				<div className='px-4 pb-2'>
-					<p className='pt-2 text-xl'>Laramie County Community College</p>
-					<p className='px-2 text-[#9ca89a]'>General Studies</p>
-					<p className='px-2 text-xs text-gray-500'>2005 - 2006</p>
 				</div>
 			</div>
 			<div className='p-6 pb-2'>
-				<p className=' uppercase py-2 text-xl tracking-widest text-[#62806a]'>
+				<p className=' uppercase py-2 text-lg tracking-widest text-[#628069]'>
 					Skills
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Web design
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Web development
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Responsive web design
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Graphic design
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Marketing
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> SEO
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> E-commerce
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Debugging
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Critical thinking
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Attention to detail
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Problem solving
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Multitasking
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Time management
-					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
-						<RiRadioButtonFill className='pr-1' /> Strong work ethics
 					</p>
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
 					Programming Languages
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
@@ -98,7 +70,7 @@ const Education = () => {
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
 					Libraries & Frameworks
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
@@ -114,109 +86,112 @@ const Education = () => {
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
 					Tools & Platforms
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> VS Code
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> GitHub
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Git
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Vercel
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Figma
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Invision
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Miro
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Adobe
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Notion
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Dubsado
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
+						<RiRadioButtonFill className='pr-1' /> BaseCamp
+					</p>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> WordPress
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> WooCommerce
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Squarespace
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Weebly
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Wix
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> MailChimp
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> QuickBooks
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Microsoft Office
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Google Workspace
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> AppSheet
 					</p>
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
-					Currently strengthening skills in
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
+					Currently strengthening skills
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> JavaScript
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> React
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Next.js
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Tailwind
 					</p>
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
 					Next areas of focus
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> React Native
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Web Animation
 					</p>
-					<p className='text-gray-600 py-2 flex items-center'>
+					<p className='text-gray-600 py-[5px] flex items-center'>
 						<RiRadioButtonFill className='pr-1' /> Advance in UX/UI
 					</p>
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
 					Currently Reading
 				</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
@@ -279,10 +254,33 @@ const Education = () => {
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
-					Current Courses
-				</p>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>Current Courses</p>
 				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
+					<div className='py-2 col-span-2'>
+						<p className='text-gray-600 flex items-center'>
+							<RiRadioButtonFill className='pr-1' /> Front-End Web Developer
+							Program
+						</p>
+						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
+							by edX - W3Cx
+						</p>
+					</div>
+					<div className='py-2 col-span-2'>
+						<p className='text-gray-600 flex items-center'>
+							<RiRadioButtonFill className='pr-1' /> Cybersecurity Fundamentals
+							Program
+						</p>
+						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
+							by edX - IBM
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className='p-6'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>
+					Previous Courses
+				</p>
+				<div className='px-4 grid justify-items-start grid-cols-3 gap-x-4'>
 					<div className='py-2'>
 						<p className='text-gray-600 flex items-center'>
 							<RiRadioButtonFill className='pr-1' /> Foundations
@@ -291,65 +289,55 @@ const Education = () => {
 							by The Odin Project
 						</p>
 					</div>
-					<div className='py-2'>
+					<div className='grid grid-cols-1 col-span-3 sm:col-span-1 md:col-span-1 lg:col-span-3 py-2'>
 						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> Full Stack Javascript
-						</p>
-						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
-							by The Odin Project
-						</p>
-					</div>
-				</div>
-			</div>
-			<div className='p-6'>
-				<p className=' uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
-					Previous Courses
-				</p>
-				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
-					<div className='grid grid-cols-1 lg:col-span-2 py-2'>
-						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> React Fundamentals
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />{" "}
+							React Fundamentals
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
 							by Udemy
 						</p>
 					</div>
-					<div className='grid grid-cols-1 lg:col-span-2 py-2'>
+					<div className='grid grid-cols-1 col-span-3 sm:col-span-2 md:col-span-1 lg:col-span-3 py-2'>
 						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> React JS for Beginners
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />{" "}
+							React JS for Beginners
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
 							by Udemy
 						</p>
 					</div>
-					<div className='grid grid-cols-1 lg:col-span-2 py-2'>
+					<div className='grid grid-cols-1 col-span-3 sm:col-span-1 md:col-span-1 lg:col-span-3 py-2'>
 						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> JavaScript Essentials
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />{" "}
+							JavaScript Essentials
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
 							by Udemy
 						</p>
 					</div>
-					<div className='grid grid-cols-1 lg:col-span-2 py-2'>
+					<div className='grid grid-cols-1 col-span-3 sm:col-span-1 md:col-span-1 lg:col-span-3 py-2'>
 						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> Python Bootcamp
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />{" "}
+							Python Bootcamp
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
 							by Udemy
 						</p>
 					</div>
-					<div className='grid grid-cols-1 lg:col-span-2 py-2'>
+					<div className='grid grid-cols-1 col-span-3 sm:col-span-2 md:col-span-2 lg:col-span-3 py-2'>
 						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> Web Development Bootcamp
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />{" "}
+							Web Development Bootcamp
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
 							by Udemy
 						</p>
 					</div>
-					<div className='grid grid-cols-1 col-span-2 py-2'>
-						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='pr-1' /> CS50&apos;s Introduction to
-							Computer Science
+					<div className='grid grid-cols-1 col-span-3 sm:col-span-2 md:col-span-2 lg:col-span-3 py-2'>
+						<p className='text-gray-600 flex items-start'>
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />{" "}
+							CS50&apos;s Introduction to Computer Science
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
 							by edX - HarvardX
@@ -358,11 +346,17 @@ const Education = () => {
 				</div>
 			</div>
 			<div className='p-6'>
-				<p className='w-full uppercase pt-2 text-transparent bg-clip-text bg-gradient-to-r text-[#9ca89a]'>
-					Future Courses
-				</p>
-				<div className='px-4 grid justify-items-start grid-cols-1 lg:grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-x-4'>
-					<div className='grid grid-cols-1 py-2'>
+				<p className='uppercase pt-2 text-lg text-[#628069]'>Future Courses</p>
+				<div className='px-4 grid justify-items-start grid-cols-2 lg:grid-cols-2 md:grid-cols-4 sm:grid-cols-2 gap-x-4'>
+					<div className='grid grid-cols-1 col-span-2 py-2'>
+						<p className='text-gray-600 flex items-center'>
+							<RiRadioButtonFill className='pr-1' /> Full Stack Javascript
+						</p>
+						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
+							by The Odin Project
+						</p>
+					</div>
+					<div className='grid grid-cols-1 col-span-2 py-2'>
 						<p className='text-gray-600 flex items-center'>
 							<RiRadioButtonFill className='flex item-start pr-1' /> Full Stack
 							Open
@@ -372,8 +366,8 @@ const Education = () => {
 						</p>
 					</div>
 					<div className='grid grid-cols-1 col-span-2 py-2'>
-						<p className='text-gray-600 flex items-center'>
-							<RiRadioButtonFill className='flex items-start pr-1' />
+						<p className='text-gray-600 flex items-start'>
+							<RiRadioButtonFill className='pr-1 pt-2 min-w-[16px] min-h-[20px]' />
 							CS50&apos;s Web Programming with Python and JavaScript
 						</p>
 						<p className='text-gray-600 text-xs italic px-4 flex items-center'>
