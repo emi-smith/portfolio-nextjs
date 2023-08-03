e2de0645b8e49901fa00ba5935a5af3cd88851ac
emi-smith <87035540+emi-smith@users.noreply.github.com>
2023-08-03 08:43:20 -0600

    Update alt description and styling

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -6,23 +6,29 @@ const ProjectItem = ({ title, technology, backgroundImg, projecturl }) => {
 	return (
 		<div
 			id='ProjectId'
-			className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#62806a] to-[#9ca89a]'
+			className='relative flex items-center justify-center h-auto w-[96%] md:w-full shadow-xl shadow-gray-400 rounded-xl p-2 md:p-4 group hover:bg-gradient-to-r from-[#628069] to-[#9ca89a]'
 		>
 			<Image
 				className='rounded-xl group-hover:opacity-10'
 				src={backgroundImg}
-				alt='/'
+				alt='Placeholder for project images, images changes on the click of a new project.'
 			/>
-			<div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-				<h3 className='text-2xl text-white tracking-wider text-center'>
-					{title}
-				</h3>
-				<p className='pb-4 pt-2 text-white text-center'>{technology}</p>
-				<Link href={projecturl}>
-					<p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
-						More Details
+			<div className='hidden h-full w-full group-hover:flex absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+				<div className='flex flex-col justify-center content-center h-full w-full'>
+					<h3 className='text-[18px] sm:text-[32px] text-white tracking-wider text-center'>
+						{title}
+					</h3>
+					<p className='pb-4 pt-2 text-[14px] sm:text-[20px] text-white text-center'>
+						{technology}
 					</p>
-				</Link>
+					<div className='flex justify-center content-center w-full h-14'>
+						<Link className='flex justify-center w-full' href={projecturl}>
+							<p className='self-center justify-self-center hover:scale-110 w-[75%] sm:w-[46%] lg:w-2/5 h-4/5 tracking-[1.5px] text-center py-2.5 px-2 rounded-lg bg-white text-[#628069] hover:text-[#9ca89a] font-medium text-base cursor-pointer hover:font-semibold'>
+								More Details
+							</p>
+						</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
