6ac0a3cfb57689fbf5fe1ede8eccf82f38b15dcf
emi-smith <87035540+emi-smith@users.noreply.github.com>
2023-08-03 08:55:50 -0600

    Update alt description and styling

--- pages/ems-shore.jsx
+++ pages/ems-shore.jsx
@@ -4,7 +4,7 @@ import EsLandingPage from "../public/assets/projects/es-home-web.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
-const stickyNotes = () => {
+const emsShore = () => {
 	return (
 		<div className='w-full'>
 			<div className='w-screen h-[50vh] relative'>
@@ -13,17 +13,21 @@ const stickyNotes = () => {
 					className='object-cover w-full h-[50vh] absolute z-1'
 					Layout='fill'
 					src={EsLandingPage}
-					alt='/'
+					alt='A image of a website with pictures of leather wrap bracelets.'
 				/>
 				<div className='absolute top-[70%] max-w-[1240px] px-4 w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-					<h2 className='py-2'>Em&apos;s Shore Landing Page</h2>
-					<h3>HTML / CSS / JavaScript</h3>
+					<h2 className='py-2 text-[22px] sm:text-[32px]'>
+						Em&apos;s Shore Landing Page
+					</h2>
+					<h3 className='py-2 text-[12px] sm:text-[16px]'>
+						HTML / CSS / JavaScript
+					</h3>
 				</div>
 			</div>
 
-			<div className='max-w-[1240px] mx-auto p-2 px-4 grid md:grid-cols-5 gap-8 py-8'>
+			<div className='max-w-[1240px] mx-auto justify-items-center p-2 px-4 grid md:grid-cols-5 gap-8 py-8'>
 				<div className='col-span-4'>
-					<p className=' uppercase py-2 text-xl tracking-widest text-[#62806a]'>
+					<p className=' uppercase py-2 text-lg tracking-widest text-[#628069]'>
 						Project
 					</p>
 					<h2 className='pb-2'>Overview</h2>
@@ -36,51 +40,64 @@ const stickyNotes = () => {
 						of the HTML and CSS skills I&apos;ve learned.
 					</p>
 
-					<div className='grid grid-cols-2 justify-items-center pt-5 pb-10'>
+					<div className='grid grid-cols-2 justify-items-center gap-10 w-full pt-5 pb-10 my-6'>
 						<a
+							className='justify-self-center w-[100%] md:w-[50%] hover:scale-105 ease-in-out duration-300'
 							href='https://github.com/emi-smith/ems-shore-landing-page'
 							target='_blank'
 							rel='noreferrer'
 						>
-							<button className='px-8 py-2 mt-4 mr-8'>View Code</button>
+							<button className='w-full text-center p-2 text-sm hover:font-500'>
+								View Code
+							</button>
 						</a>
 						<a
+							className='justify-self-center w-[100%] md:w-[50%] hover:scale-105 ease-in-out duration-300'
 							href='https://ems-shore-landing-page.vercel.app/'
 							target='_blank'
 							rel='noreferrer'
 						>
-							<button className='px-8 py-2 mt-4'>View Project</button>
+							<button className='w-full text-center p-2 text-sm hover:font-500'>
+								View Project
+							</button>
 						</a>
 					</div>
 				</div>
-				<div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+				<div className='col-span-4 md:col-span-1 w-[75%] sm:w-[100%] shadow-xl shadow-gray-400 rounded-xl py-4'>
 					<div className='p-4'>
 						<p className='text-center font-bold pb-2'>Technologies</p>
-						<div className='grid grid-cols-3 md:grid-cols-1'>
+						<div className='grid grid-cols-1'>
 							<p className='text-gray-600 py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> HTML
+								<RiRadioButtonFill className='pr-1' size='14' color='#628069' />{" "}
+								HTML
 							</p>
 							<p className='text-gray-600 py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> CSS
+								<RiRadioButtonFill className='pr-1' size='14' color='#628069' />{" "}
+								CSS
 							</p>
 							<p className='text-gray-600 py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> Javascript
+								<RiRadioButtonFill className='pr-1' size='14' color='#628069' />
+								JavaScript
 							</p>
 							<p className='text-gray-600 py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> Git
+								<RiRadioButtonFill className='pr-1' size='14' color='#628069' />{" "}
+								Git
 							</p>
 							<p className='text-gray-600 py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> GitHub
+								<RiRadioButtonFill className='pr-1' size='14' color='#628069' />{" "}
+								GitHub
 							</p>
 						</div>
 					</div>
 				</div>
 				<Link href='/#projects'>
-					<p className='cursor-pointer text-[#62806a]'>Back</p>
+					<p className='cursor-pointer text-[#628069] hover:text-[#9ca89a]'>
+						Back
+					</p>
 				</Link>
 			</div>
 		</div>
 	);
 };
 
-export default stickyNotes;
+export default emsShore;
